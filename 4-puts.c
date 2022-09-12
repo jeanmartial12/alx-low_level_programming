48bbc13d2e84ddad52f6f307c6900c12b6aca54e
jeanmartial12 <jeancragbe@gmail.com>
2022-09-12 06:31:42 -0700

    coding

--- 4-puts.c
+++ 4-puts.c
@@ -1,23 +1,14 @@
-include <stdio.h>                                                    
+#include <stdio.h>
 
-                                                                     
-
-/**                                                                  
-
-* main - print the string in the put function                        
-
-*                                                                    
-
-*Description : using the main function                               
-
-* this program prints "Programming is like building"                 
-
-* Return: 0                                                          
-
-*/                                                                   
-
-int main(void)   
+/**
+* main - print the string in the put function
+* 
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/ 
+int main(void)
 {
-	puts("\"Programming is like building"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
