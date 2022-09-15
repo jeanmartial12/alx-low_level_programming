1d3f8905efc47bf50a5756b801b7280c47167af6
jeanmartial12 <jeancragbe@gmail.com>
2022-09-15 08:38:32 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 /**
 *main - header file for protoypes
 *void protoypes (void)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,17 +1,47 @@
-#include "main.h"
+fndef MAIN_H
+
+#define MAIN_H
+
+
 
 /**
-*_isupper - checks if a  character is uppercase or not
-*@c: character to be tested
-*Return: 1 whether it is, 0 otherwise
-*/
+ *
+ * *main - header file for protoypes
+ *
+ * *void protoypes (void)
+ *
+ * *int protoypes (int)
+ *
+ * */
 
-int _isupper(int c)
-{
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		return (1);
-	}
 
-	return (0);
-}
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
+
+int _putchar(char);
+
+int _isupper(int);
+
+int _isdigit(int);
+
+int mul(int, int);
+
+
+
+#endif /* MAIN_H *
+
+
