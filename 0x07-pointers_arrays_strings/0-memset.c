1d6233fbda4f020a36730c1fe0d7a75faf7cb419
jeanmartial12 <jeancragbe@gmail.com>
2022-09-26 06:33:53 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-/*
+/* 
  * File: 0-memset.c
  * Auth: not
  */
@@ -11,17 +11,18 @@
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
+ * description _memset: over there
  *
  * Return: A pointer to the filled memory area @s.
  *
  */
 void *_memset(void *s, int c, size_t n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+    unsigned int index;
+    unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)
-	      memory[index] = value;
+    for (index = 0; index < n; index++)
+	memory[index] = value;
 
-	return (memory);
+    return (memory);
 }
