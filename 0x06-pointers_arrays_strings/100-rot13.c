082952cba11819e2e8a1fe604eaf427683f1030d
jeanmartial12 <jeancragbe@gmail.com>
2022-09-27 07:26:50 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,45 @@
 #include "main.h"
+
 /**
- * rot13 - convert to rot 13
- * @str: input string
- * Return: String conversion
+ * rot13 - Encodes a string using rot13.
+ *
+ * Return: A pointer to the encoded string.
  */
 char *rot13(char *str)
 {
-	int count = 0, x;
-	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R',
+			'S', 'T', 'U', 'V', 'W', 'X',
+			'Y', 'Z', 'a', 'b', 'c', 'd',
+			'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm', 'n', 'o', 'p',
+			'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z', 'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J', 'K',
+			'L', 'M', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w',
+			'x', 'y', 'z', 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm'};
 
-	while (str[count] != '\0')
+	while (str[indx1])
 	{
-		x = 0;
-		while (letters[x] != '\0')
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if (str[count] == letters[x])
+			if (str[indx1] == alphabet[indx2])
 			{
-				str[count] = rot13[x];
+				str[indx1] = rot13key[indx2];
 				break;
 			}
-			x++;
 		}
-		count++;
+
+		indx1++;
 	}
+
 	return (str);
 }
