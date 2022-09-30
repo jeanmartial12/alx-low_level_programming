e028063ec94841645581a22c9248211cd4e78098
CRAGBE BOMBRO JEAN MARTIAL <36504614+jeanmartial12@users.noreply.github.com>
2022-09-30 22:31:15 +0000

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,23 +1,17 @@
-#include <stdio.h>                                                                                                                   
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * main - Print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
-                                                                                                                                     
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-                                                                                                                                     
-/*Declaring variables*/                                                                                                              
-int count = 0;                                                                                                                       
-                                                                                                                                     
-if (argc > 0)                                                                                                                        
-{                                                                                                                                    
-/*WHILE - Print each arguments*/                                                                                                     
-while (count < argc)                                                                                                                 
-{                                                                                                                                    
-printf("%s\n", argv[count]);
+#include <stdio.h> 
+ /** 
+  * main - Program that prints the number of arguments passed into it 
+  * @argc: - Int of arguments passed into program including command 
+  * @argv: - Array of pointers to the strings of arguments passed 
+  * Return: 0 
+  */ 
+  
+ int main(int argc, char **argv) 
+ { 
+         int i; 
+  
+         for (i = 0; argv[i] != '\0'; argc--, i++) 
+                 printf("%s\n", argv[i]); 
+  
+         return (0); 
+ }
